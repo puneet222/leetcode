2f34b0561ec610683357f8a3f84049bb44c92189
puneet222 <aggarwal.puneet222@gmail.com>
2020-09-03 22:24:06 +0530

    Update Greedy Easy 860

--- Easy/Q-860.js
+++ Easy/Q-860.js
@@ -3,27 +3,25 @@
  * @return {boolean}
  */
 var lemonadeChange = function (bills) {
-    let map = new Map();
-    map.set(5, 0);
-    map.set(10, 0);
-    map.set(20, 0);
+    let five = 0;
+    let ten = 0;
     for (let i = 0; i < bills.length; i++) {
         if (bills[i] === 5) {
-            map.set(5, map.get(5) + 1);
+            five++;
         }
         else if (bills[i] === 10) {
-            if (map.get(5) > 0) {
-                map.set(5, map.get(5) - 1);
-                map.set(10, map.get(10) + 1);
+            if (five > 0) {
+                five--;
+                ten++;
             } else {
                 return false;
             }
         } else {
-            if (map.get(10) > 0 && map.get(5) > 0) {
-                map.set(5, map.get(5) - 1);
-                map.set(10, map.get(10) - 1);
-            } else if (map.get(5) >= 3) {
-                map.set(5, map.get(5) - 3);
+            if (ten > 0 && five > 0) {
+                five--;
+                ten--;
+            } else if (five >= 3) {
+                five = five - 3;
             } else {
                 return false;
             }
