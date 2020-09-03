86570ae018f3875048723e6285c3f8672d9531b1
puneet222 <aggarwal.puneet222@gmail.com>
2020-09-03 12:59:31 +0530

    Greedy Easy 1217

---
+++ Easy/Q-1217.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} position
+ * @return {number}
+ */
+var minCostToMoveChips = function (position) {
+    let even = 0, odd = 0;
+    for (let i = 0; i < position.length; i++) {
+        if (position[i] % 2 === 0) {
+            even++;
+        } else {
+            odd++;
+        }
+    }
+    return Math.min(even, odd);
+};
